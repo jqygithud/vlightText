2a1dd3614f8fa2d9e57024237b1913218c11110c
jqy <632624982@qq.com>
2023-11-15 15:42:08 +0800

    Initial commit

---
+++ Example/vlightText/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  vlightText
+//
+//  Created by 632624982@qq.com on 11/15/2023.
+//  Copyright (c) 2023 632624982@qq.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
